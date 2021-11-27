7d8bbf1dbf757f36cc5561fda41e8848d1b16b7c
pademss <fatma.putri@ui.ac.id>
2021-11-27 11:15:18 +0700

    CS MODUL9 FATMA

--- app/src/main/java/fatmaJmartKD/jmart_android/MainActivity.java
+++ app/src/main/java/fatmaJmartKD/jmart_android/MainActivity.java
@@ -3,12 +3,16 @@ package fatmaJmartKD.jmart_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main = (TextView) findViewById(R.id.textView3);
     }
 }
--- app/src/main/java/fatmaJmartKD/jmart_android/RegisterActivity.java
+++ app/src/main/java/fatmaJmartKD/jmart_android/RegisterActivity.java
@@ -1,8 +1,20 @@
 package fatmaJmartKD.jmart_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import fatmaJmartKD.jmart_android.request.RegisterRequest;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +22,35 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        EditText nameReg = findViewById(R.id.editTextTextPersonName);
+        EditText emailReg = findViewById(R.id.editTextTextEmailAddress2);
+        EditText passwordText = findViewById(R.id.editTextTextPassword2);
+        Button registerButton = findViewById(R.id.button2);
+
+        registerButton.setOnClickListener(v -> {
+            Response.Listener<String> listener = response -> {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if(jsonObject != null) {
+                        Toast.makeText(RegisterActivity.this, "Register successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                } catch (JSONException err){
+                    Toast.makeText(RegisterActivity.this, err.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            };
+            Response.ErrorListener errorListener = response -> {
+                Toast.makeText(RegisterActivity.this, response.getMessage(), Toast.LENGTH_SHORT).show();
+            };
+            String name = nameReg.getText().toString();
+            String email = emailReg.getText().toString();
+            String password = passwordText.getText().toString();
+            RegisterRequest registerRequest = new RegisterRequest(name, email, password, listener, errorListener);
+            RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+            requestQueue.add(registerRequest);
+
+        });
     }
 }
